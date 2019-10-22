5fa3278c8241e95c1b9150966d59b288a314621d
Per Van Dyke <pav17@hampshire.edu>
2019-10-22 10:58:55 -0400

    Removed unused imports

--- src/main/java/edu/smith/cs/csc212/lists/SinglyLinkedList.java
+++ src/main/java/edu/smith/cs/csc212/lists/SinglyLinkedList.java
@@ -2,8 +2,6 @@ package edu.smith.cs.csc212.lists;
 
 import me.jjfoley.adt.ListADT;
 import me.jjfoley.adt.errors.BadIndexError;
-import me.jjfoley.adt.errors.EmptyListError;
-import me.jjfoley.adt.errors.TODOErr;
 
 /**
  * A Singly-Linked List is a list that has only knowledge of its very first
